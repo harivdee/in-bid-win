cb1d8ce6d866d60e2b28a36348fd8f523f5936cf
George Lardas <georgel.1986@gmail.com>
2019-12-27 15:41:28 +0200

    Login Page Built
    Built login and register forms for website

---
+++ C - Processing/Frontend/js/login.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+	$('ul.switcher li').click(function(){
+		let tab_id = $(this).attr('data-tab');
+
+		$('li').removeClass('active');
+    $('div.tab-pane').removeClass('active');
+
+		$(this).addClass('active');
+		$("#"+tab_id).addClass('active');
+	})
+
+})
