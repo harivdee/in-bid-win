6e32b3bed38b5367fedf695b1c5f53a6ed56ba38
zerokatana <christopher.kamp@hotmail.com>
2020-01-20 19:36:08 +0200

    redirect fix

--- C - Processing/Backend/auction/src/main/java/com/nullcorp/auction/controller/AuthenticateController.java
+++ C - Processing/Backend/auction/src/main/java/com/nullcorp/auction/controller/AuthenticateController.java
@@ -4,11 +4,6 @@ package com.nullcorp.auction.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-<<<<<<< HEAD
-=======
-
->>>>>>> login-redirect
-
 
 @Controller
 public class AuthenticateController {
@@ -18,15 +13,9 @@ public class AuthenticateController {
         return "login";
     }
     
-<<<<<<< HEAD
-    @PostMapping("/loginPage")
-    public String takeMeHome(){
-        return "/";
-=======
     @PostMapping("/")
     public String loginSuccess(){
         return "home";
->>>>>>> login-redirect
     }
     
     @GetMapping("/access-denied")
