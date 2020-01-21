060f37db98e289b6ddf1d06be4b7e28ffbb31669
Konstantinos Spiliopoulos <pirosvestiras@gmail.com>
2020-01-21 12:48:49 +0200

    fixed create item redirect

--- C - Processing/Backend/auction/src/main/java/com/nullcorp/auction/controller/ImageController.java
+++ C - Processing/Backend/auction/src/main/java/com/nullcorp/auction/controller/ImageController.java
@@ -6,6 +6,8 @@ import com.nullcorp.auction.service.HouseService;
 import com.nullcorp.auction.service.ImageService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,29 +17,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 @RequestMapping("/image")
 public class ImageController {
-    
+
     @Autowired
     ImageService imService;
     @Autowired
     HouseService hService;
-    
-   
-    
+
     @GetMapping("/list")
-    public String listImages(@RequestParam("houseId") Integer id, Model m){
+    public String listImages(@RequestParam("houseId") Integer id, Model m) {
         List<Image> list = imService.getAllImagesByHouse(id);
         House h = hService.getHouseById(id);
         m.addAttribute("listOfImages", list);
         m.addAttribute("house", h);
         return "listImages";
     }
+
     @GetMapping("/upload")
     public String getImageForm(@ModelAttribute("image") Image i,
-            @RequestParam("hid") Integer id, Model m){
+            @RequestParam("hid") Integer id, Model m) {
         House h = hService.getHouseById(id);
         m.addAttribute("house", h);
         return "formImage";
@@ -46,11 +46,12 @@ public class ImageController {
     @PostMapping("/upload")
     public String uploadImages(@ModelAttribute("image") Image i,
             @RequestParam("houseId") Integer id,
-            BindingResult result){
+            BindingResult result, Model m) {
         i.setHouse(hService.getHouseById(id));
         imService.createOrUpdateImage(i);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         
-        return "redirect:/user/list";
-        
+        return "redirect:/item/listByUsername?username="+auth.getName();
+
     }
 }
